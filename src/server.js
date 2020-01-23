5e5716d54aa0ea0d35ce0e225169b150e1f81c8f
Pablo Peña <pablo@makeitworth.com.mx>
2020-01-22 20:56:52 -0600

    Update: Up to lessong 'route order'
    	So far we have learned the types of routes:
    		* exact match
    		* regex match
    		* param match
    		* glob match
    
    	and what are middlewares for.

--- src/server.js
+++ src/server.js
@@ -12,4 +12,35 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+	console.log('logging')
+	req.mydata = 'hello11'
+	next()
+}
+
+app.get('/data', log, (req, res) => {
+	res.send({ message: req.mydata })
+})
+
+app.post('/data', (req, res) => {
+	console.log(req.body)
+	res.send({ message: 'okay' })
+})
+
+app.get(/^(me)[A-Za-z]*/, log, (req, res) => {
+	res.send({ message: 'entrando a me' })
+})
+
+app.get('/user*', log, (req, res) => {
+	res.send({ message: req.route })
+})
+
+app.get('/:id/nice', log, (req, res) => {
+	res.send({ message: req.params })
+})
+
+export const start = () => {
+	app.listen(3000, () => {
+		console.log('server is on port 3000')
+	})
+}
